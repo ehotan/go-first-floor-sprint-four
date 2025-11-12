cebc32f85081032eeafa2906550d807bf341076a
Artem <ehotan@yandex.ru>
2025-11-12 22:24:53 +0300

    remove last TODO

--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -104,7 +104,6 @@ func RunningSpentCalories(steps int, weight, height float64, duration time.Durat
 }
 
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: обработать ошибки, разбить мат. действия на строки
 	if duration <= 0 {
 		return 0, errors.New("ошибка ввода данных, тренировки: продолжительность должна быть положительной")
 	}
